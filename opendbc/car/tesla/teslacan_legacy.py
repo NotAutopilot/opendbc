8d98f237d267a12a82f59e3d86aa37afab609bc1
Vehicle Researcher <user@comma.ai>
2025-11-30 14:44:43 -0500

    Use Bus 2 for Pre-AP pedal command

--- opendbc/car/tesla/teslacan_legacy.py
+++ opendbc/car/tesla/teslacan_legacy.py
@@ -60,49 +60,12 @@ class TeslaCANRaven:
 class TeslaCANPreAP(TeslaCANRaven):
   def create_pedal_command(self, pedal, idx):
     # GAS_INTERCEPTOR 0x551
-    # Use CANBUS.party (0) for Pre-AP
+    # Use CANBUS.autopilot_party (2) for Pre-AP pedal command
     values = {
       "ENABLE": 1,
       "GAS_COMMAND": pedal, # 0..100? or scaled? Check DBC. Assuming raw value.
       "COUNTER": idx,
     }
-    # Checksum is usually calculated by packer if sig is defined, or we do it manually?
-    # If DBC has Checksum signal, packer might expect us to provide it if it's not auto-calculated?
-    # Usually openpilot packers don't auto-calc checksums unless specified.
-    # Tesla checksum is standard. 
-    # But GAS_INTERCEPTOR might be different.
-    # Tinkla's safety checks byte 5 for enable.
-    # If we assume standard packing, we can use make_can_msg.
-    # But for checksum, we might need to calc it.
-    # Let's check if we can inspect the packed data.
     
-    msg = self.packers[CANBUS.party].make_can_msg("GAS_INTERCEPTOR", CANBUS.party, values)
-    # GAS_INTERCEPTOR might typically have a checksum. 
-    # If so, we need to calculate it.
-    # Assuming standard Tesla checksum over the payload.
-    # But msg is (addr, data, bus).
-    # data is bytes.
-    # We can recalc checksum and update.
-    # However, we need to know the Checksum signal name to update `values` and repack.
-    # Or modify bytes directly.
-    
-    # Tinkla safety doesn't check checksum for GAS_INTERCEPTOR in rx_hook?
-    # It checks 0x552 (GAS_SENSOR).
-    # For TX (0x551), it just forwards or checks limits.
-    # Wait, safety_tesla.h doesn't enforce checksum on 0x551 TX? 
-    # It just checks logic.
-    
-    # I will assume standard Tesla checksum logic applies if the signal exists.
-    # I'll try to calculate it.
-    # But I don't know the signal name. "CHECKSUM"? "Checksum"?
-    # I'll skip checksum calculation for now and rely on packer or assume it's not needed/checked by Pedal firmware?
-    # Pedal firmware usually checks checksum.
-    # I'll try to add "CHECKSUM" to values.
-    
-    # Let's assume signal is "CHECKSUM".
-    # values["CHECKSUM"] = 0
-    # data = ...
-    # values["CHECKSUM"] = self.checksum(0x551, data[:-1]) # Checksum is usually last byte
-    
-    # Since I can't see DBC, I'll trust the packer or just send as is.
+    msg = self.packers[CANBUS.autopilot_party].make_can_msg("GAS_INTERCEPTOR", CANBUS.autopilot_party, values)
     return msg
