f9cb67343b339ca540cc4b42ef67ae6a95ef4fda
jjackbrandt <jrb4209@gmail.com>
2026-08-17 02:17:12 +0000

    Add Tesla Pre-AP to vehicle selector

--- opendbc/sunnypilot/car/tests/test_car_list.py
+++ opendbc/sunnypilot/car/tests/test_car_list.py
@@ -11,3 +11,13 @@ class TestCarList(unittest.TestCase):
       current_car_list = f.read()
 
     assert generated_car_list == current_car_list, "Run opendbc/sunnypilot/car/platform_list.py to update the car list"
+
+  def test_tesla_preap_selector_entry(self):
+    assert get_car_list()["Tesla Model S (Pre-AP) 2012-14"] == {
+      "platform": "TESLA_MODEL_S_PREAP",
+      "make": "Tesla",
+      "brand": "tesla",
+      "model": "Model S (Pre-AP)",
+      "year": ["2012", "2013", "2014"],
+      "package": "All",
+    }
