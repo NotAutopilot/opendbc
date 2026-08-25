1e8af832d6aff8b83cec633f1ea504c08e17d3e2
jjackbrandt <jrb4209@gmail.com>
2026-08-25 03:38:35 +0000

    tesla_preap: add NAPRadarHud param key
    UI overlay toggle. Default off.

--- opendbc/car/tesla/preap/nap_params.py
+++ opendbc/car/tesla/preap/nap_params.py
@@ -24,6 +24,7 @@ class NAPParamKeys:
 
   # Radar
   RADAR_ENABLED = "NAPRadarEnabled"
+  RADAR_HUD = "NAPRadarHud"
   RADAR_IGNORE_HW_FAIL = "NAPRadarIgnoreHwFail"
   RADAR_BEHIND_NOSECONE = "NAPRadarBehindNosecone"
   RADAR_OFFSET = "NAPRadarOffset"
@@ -54,6 +55,7 @@ DEFAULTS = {
   NAPParamKeys.PEDAL_CALIB_FACTOR: 1.0,
   NAPParamKeys.PEDAL_CALIB_ZERO: 0.0,
   NAPParamKeys.RADAR_ENABLED: False,
+  NAPParamKeys.RADAR_HUD: False,
   NAPParamKeys.RADAR_IGNORE_HW_FAIL: False,
   NAPParamKeys.RADAR_BEHIND_NOSECONE: False,
   NAPParamKeys.RADAR_OFFSET: 0.0,
